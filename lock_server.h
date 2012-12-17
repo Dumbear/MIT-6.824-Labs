14004523458649a1ca9e71cc149573462b993b59
Liang Yaxiong <dumbear@163.com>
2012-12-17 15:36:31 +0800

    Lab 1 - Step One Passed

--- lock_server.h
+++ lock_server.h
@@ -5,6 +5,7 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
@@ -13,11 +14,17 @@ class lock_server {
 
  protected:
   int nacquire;
+  /* mutex for $lock_table */
+  pthread_mutex_t lock_table_mutex;
+  /* table of all the locks, each maps to a single mutex */
+  std::map<lock_protocol::lockid_t, pthread_mutex_t *> lock_table;
 
  public:
   lock_server();
-  ~lock_server() {};
+  ~lock_server();
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, lock_protocol::status &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, lock_protocol::status &);
 };
 
 #endif 
